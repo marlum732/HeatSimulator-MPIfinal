21cd0db1524779aa4098f63321eaa4d9aa606ff4
marlum732 <marko.lumbar@gmail.com>
2023-09-07 16:16:40 +0200

    MPI now works

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,15 @@ public class Main {
         int nPoints = 1000;
         int seed = 234;
 
+        long startTime = System.currentTimeMillis();
 
         int me = MPI.COMM_WORLD.Rank();
         int size = MPI.COMM_WORLD.Size();
 
-        System.out.println("Rank: " + me);
+        int chunkSize = width / (size-1);
+        double[] sendBuffer = new double[chunkSize * height];
+        double[] receivedBuffer = new double[chunkSize*height];
+        double[][] chunk = new double[chunkSize][height];
 
         double[][] temperature = new double[width][height];
         if (me==0){
@@ -25,15 +29,9 @@ public class Main {
                 int y = random.nextInt(height);
                 temperature[x][y] = 1;
             }
-            System.out.println("Matrix");
-        }
-        
-        int chunkSize = width / (size-1);
-        double[] sendBuffer = new double[chunkSize * height];
-        double[] receivedBuffer = new double[chunkSize*height];
+
 
         //chunk distribution-breaking temperature array
-        if (me == 0) {
             for (int rank = 1; rank<size ; rank++) { //all other workers
                 int startRow = (rank-1) * chunkSize;
 
@@ -46,13 +44,68 @@ public class Main {
                 }
                 MPI.COMM_WORLD.Send(sendBuffer, 0, chunkSize*height, MPI.DOUBLE, rank, 0);
             }
-            System.out.println("Data distributed");
         }else {
             //receiving a message from all non zero rank processes to store data in receivedBuffer
             MPI.COMM_WORLD.Recv(receivedBuffer, 0, chunkSize*height, MPI.DOUBLE, 0,0);
-            System.out.println("received data chunk");
+
+            int index=0;
+            for (int i = 0; i < chunkSize; ++i) {
+                for (int j = 0; j < height; ++j) {
+                    chunk[i][j] = receivedBuffer[index++];
+                }
+            }
+
+            boolean localStable = false;
+            while (!localStable) {
+
+                localStable = true;
+
+                for (int i = 0; i < chunkSize; ++i) {
+                    for (int j = 0; j < height; ++j) {
+                        if (temperature[i][j] == 1) continue;
+
+                        double value = 0;
+                        if (i > 0) value += temperature[i - 1][j];
+                        if (i < chunk[0].length - 1) value += temperature[i + 1][j];
+                        if (j > 0) value += temperature[i][j - 1];
+                        if (j < chunk.length - 1) value += temperature[i][j + 1];
+                        value /= 4;
+
+                        double diff = Math.abs(value - temperature[i][j]);
+                        if (diff > 0.0025) {
+                            localStable = false;
+                        }
+                        temperature[i][j] = value;
+                    }
+                }
+            }
+
+            index = 0;
+            for (int i = 0; i < chunkSize; ++i) {
+                for (int j = 0; j < height; ++j) {
+                    sendBuffer[index++] = chunk[i][j];
+                }
+            }
+
+            //updated chunk back to rank 0
+            MPI.COMM_WORLD.Send(sendBuffer, 0, chunkSize * height, MPI.DOUBLE, 0, 0);
         }
-        
+
+        if (me == 0) {
+            //merging updated chunks
+            for (int rank = 1; rank < size; ++rank) {
+                int startRow = (rank - 1) * chunkSize;
+                MPI.COMM_WORLD.Recv(receivedBuffer, 0, chunkSize * height, MPI.DOUBLE, rank, 0);
+
+                int index = 0;
+                for (int i = 0; i < chunkSize; ++i) {
+                    for (int j = 0; j < height; ++j) {
+                        temperature[startRow + i][j] = receivedBuffer[index++];
+                    }
+                }
+            }
+            System.out.println("Execution time: " + (System.currentTimeMillis() - startTime) + " ms");
+        }
 
         MPI.Finalize();
     }
